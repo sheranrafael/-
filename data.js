487c2007340c583480ae04895331c469cd22eb55
Rafael Sheran <154270131+sheranrafael@users.noreply.github.com>
2024-12-03 22:15:41 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'Peaky Blinders: Sangue, Apostas e Navalhas',
+        des: 'Os Peaky Blinders são uma organização criminosa de origem cigana que se passa na cidade de Birmingham, Inglaterra, em 1919, formada vários meses após o final da Primeira Guerra Mundial (1914–1918). A história é centrada na ambição do líder da gangue inglesa, Thomas Shelby.',
+        image: 'images/slider 1.PNG'
+        
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
+    }
+]
